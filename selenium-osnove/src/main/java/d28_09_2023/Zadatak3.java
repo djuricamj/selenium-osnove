3084314b164d7cad94e74e25c159c1410b0d9b3f
Marijana Djurica <djuricamj@gmail.com>
2023-10-01 14:43:14 +0200

    Debug already existing code

---
+++ selenium-osnove/src/main/java/d28_09_2023/Zadatak3.java
@@ -0,0 +1,45 @@
+package d28_09_2023;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Zadatak3 {
+//    3. Zadatak
+//Za zadatak iz foldera za domaci izdebagirati i ispraviti da radi.
+public static void main(String[] args) throws InterruptedException {
+
+
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    driver.get("http://seleniumdemo.com/?product=git-basics");
+
+//    WebElement cartProductCount = driver.findElement(By.cssSelector(".nav__woocart .count"));
+    WebElement addToCartBtn = driver.findElement(By.name("add-to-cart"));
+
+    addToCartBtn.click();
+
+    Thread.sleep(1000); // cekaj da se osvezi stranica
+
+//    wait.until(ExpectedConditions.textToBePresentInElement(cartProductCount, "1"));
+
+    wait
+            .withMessage("Tekst se nije pojavio")
+            .until(ExpectedConditions.textToBe(By.cssSelector(".nav__woocart .count"), "1"));
+
+
+    System.out.println("KRAJ");
+
+    driver.quit();
+}
+}
